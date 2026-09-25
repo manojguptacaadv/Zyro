a4f586057149b1a38af6a3392045030410a07583
manojguptacaadv <manojgupta.caadv@gmail.com>
2026-09-25 11:07:27 +0530

    Add MainActivity class for Capacitor app

--- android/app/src/main/java/com/zyro/app/MainActivity.java
+++ android/app/src/main/java/com/zyro/app/MainActivity.java
@@ -1,1 +1,11 @@
+package com.zyro.app;
 
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
